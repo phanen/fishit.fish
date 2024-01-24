4da9336bf22d16330b19da3f021af37d5e2984e8
phanium <nxhsdu@qq.com>
2024-01-24 22:15:08 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## fishit.fish
+* Pakcage in fish way.
+* Work with fisher.
+
+## example
+* <https://github.com/phanen/zoxide.fish>
+```fish
+zoxide init --no-cmd fish | fishit zoxide.fish
+fisher install ./zoxide.fish
+```
---
+++ functions/fishit.fish
@@ -0,0 +1,29 @@
+function fishit
+  test (count $argv) -le 1
+  or return 1
+  set -l t1 (mktemp)
+  cat > $t1
+  or return 1
+  set -l outdir
+  if test -z "$argv"
+    set outdir "$(pwd)"
+  else
+    set outdir "$argv"
+  end
+  set -l plugname (basename (realpath $outdir))
+  rm -rv $outdir/{completions, conf.d, functions}
+  mkdir -pv $outdir/{completions, conf.d, functions}
+  set -l t2 (mktemp)
+  source $t2
+  or return 1
+  for f in $(cat $t2 | grep ^function | cut -d' ' -f2)
+    funcsave -d $outdir/functions $f
+  end
+  sed -i '/^function/,/^end/d' $t2
+  grep -q ^complete $t2
+  and grep ^complete $t2 > $outdir/completions/$plugname
+  grep -qv ^complete $t2
+  and grep -v ^complete $t2 > $outdir/conf.d/$plugname
+  rm -v $t1
+  rm -v $t2
+end
